e350083ea1cbede21d636ae2e3387e070006c2a4
MUSKAN MAHAJAN <45875834+muskanmahajan37@users.noreply.github.com>
2020-12-31 00:26:11 +0530

    Add files via upload

---
+++ Override.java
@@ -0,0 +1,34 @@
+class A
+{
+int i,j;
+A(int a,int b)
+{
+i=a;
+j=b;
+}
+void show()
+{
+System.out.println(i,j);
+}
+}
+class B extends A
+{
+int k;
+B(int a,int b, int c)
+{
+super(a,b);
+k=c;
+}
+void show()
+{
+System.out.println(k);
+}
+}
+class Override
+{
+public static void main(String arg[])
+{
+B subob=new B (1,2,3);
+subob.show();
+}
+}
